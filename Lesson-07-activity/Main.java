49604ac8a1eb7a356581424a2de2bcf47c125db3
ki0911 <jiaoqihan09@gmail.com>
2025-09-26 01:02:34 +0000

    update

--- Lesson-07-activity/Main.java
+++ Lesson-07-activity/Main.java
@@ -31,15 +31,27 @@ class Main {
     Create the variables, ask the user for the variable values, write the equation in file EQ2-act6 and display the equation value..
     
 */
+    System.out.println("Enter r");
+    double r = Input.readDouble();
+    System.out.println("Enter t");
+    double t = Input.readDouble();
+    double s = 0;
+    s = Math.pow(t,5)*Math.pow(r+2,4);
+    System.out.println(s);
 
- 
 
 /*  
     Challenge 4:
     Create the variables, ask the user for the variable values, write the equation in file EQ3-act6 and display the equation value..
     
 */
-
+    System.out.println("Enter A");
+    double A = Input.readDouble();
+    System.out.println("Enter B");
+    double B = Input.readDouble();
+    double C = 0;
+    C = Math.sqrt(A+B);
+    System.out.println(C);
 
 
 /*  
@@ -47,17 +59,29 @@ class Main {
     Create the variables, ask the user for the variable values, write the equation in file EQ4-act6 and display the equation value..
     
 */
-
-
-
+    System.out.println("Enter x2");
+    double x2 = Input.readDouble();
+    System.out.println("Enter x1");
+    double x1 = Input.readDouble();
+    System.out.println("Enter y2");
+    double y2 = Input.readDouble();
+    System.out.println("Enter y1");
+    double y1 = Input.readDouble();
+    double d = 0;
+    d = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+    System.out.println(d);
+    
 
 /*  
     Challenge 6:
     Create the variables, ask the user for the variable values, write the equation g=sin(deg) and display the equation value..
     
 */
-
-
+    System.out.println("Enter deg");
+    double deg = Input.readDouble();
+    double g = 0;
+    g = Math.sin(Math.toRadians(deg));
+    System.out.println(g);
 
 
 
@@ -66,7 +90,13 @@ class Main {
     Create the variables, ask the user for the variable values, write the equation in file EQ5-act6 and display the equation value.
     
 */
-
+    System.out.println("Enter m");
+    double m = Input.readDouble();
+    System.out.println("Enter n");
+    double n = Input.readDouble();
+    double k = 0;
+    k = Math.pow(m,5) / Math.sqrt(n+1);
+    System.out.println(k);
 
 
 
